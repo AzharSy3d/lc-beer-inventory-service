9fdf4c19c338f66acb372697218fc09a9d4c3f09
AzharSy3d <azharuworld@gmail.com>
2021-07-03 20:59:36 +0530

    01- Initial Project Setup

---
+++ src/main/java/com/azsyed/lcbeerinventoryservice/web/mappers/BeerInventoryMapper.java
@@ -0,0 +1,16 @@
+package com.azsyed.lcbeerinventoryservice.web.mappers;
+
+import com.azsyed.lcbeerinventoryservice.domain.BeerInventory;
+import com.azsyed.lcbeerinventoryservice.web.model.BeerInventoryDto;
+import org.mapstruct.Mapper;
+
+/**
+ * Created by jt on 2019-05-31.
+ */
+@Mapper(uses = {DateMapper.class})
+public interface BeerInventoryMapper {
+
+    BeerInventory beerInventoryDtoToBeerInventory(BeerInventoryDto beerInventoryDTO);
+
+    BeerInventoryDto beerInventoryToBeerInventoryDto(BeerInventory beerInventory);
+}
