e8a527bb1f239c4ead787ed45e47cb27c21fdaf9
AzharSy3d <azharuworld@gmail.com>
2021-07-12 00:59:16 +0530

    Implement New Inventory Listener, Bug Fixes etc.

--- src/main/java/com/azsyed/lcbeerinventoryservice/config/JmsConfig.java
+++ src/main/java/com/azsyed/lcbeerinventoryservice/config/JmsConfig.java
@@ -2,11 +2,16 @@ package com.azsyed.lcbeerinventoryservice.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
 
+@Configuration
 public class JmsConfig {
+
+    public static final String NEW_INVENTORY_QUEUE = "new-inventory";
+
 
     @Bean // Serialize message content to json using TextMessage
     public MessageConverter jacksonJmsMessageConverter(ObjectMapper objectMapper) {
---
+++ src/main/java/com/azsyed/lcbeerinventoryservice/services/NewInventoryListener.java
@@ -0,0 +1,28 @@
+package com.azsyed.lcbeerinventoryservice.services;
+
+import com.azsyed.common.events.NewInventoryEvent;
+import com.azsyed.lcbeerinventoryservice.config.JmsConfig;
+import com.azsyed.lcbeerinventoryservice.domain.BeerInventory;
+import com.azsyed.lcbeerinventoryservice.repositories.BeerInventoryRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class NewInventoryListener {
+
+    private final BeerInventoryRepository beerInventoryRepository;
+
+    @JmsListener(destination = JmsConfig.NEW_INVENTORY_QUEUE)
+    public void listen(NewInventoryEvent event){
+        log.info("listen GOt Inventory " +event.toString());
+        beerInventoryRepository.save(BeerInventory.builder()
+                                    .beerId(event.getBeerDto().getId())
+                                    .upc(event.getBeerDto().getUpc())
+                                    .quantityOnHand(event.getBeerDto().getQuantityOnHand())
+                                    .build());
+    }
+}
